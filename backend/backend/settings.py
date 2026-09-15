f25a0843246210044b9b6693ccf7556d4e6239fd
dive0-bit <dive9249@gmail..com>
2026-09-16 01:27:55 +0530

    Fixed ALLOWED_HOSTS for Render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-uq^wki0k50ly(*snb$()r#(!zc_)=a%y*-g0qwptkvct#@sbix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# settings.py mein
+ALLOWED_HOSTS = ['mady-a-file-organizer.onrender.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
